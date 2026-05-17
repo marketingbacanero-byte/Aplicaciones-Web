94b337af999241d537d954639997dd5222a557b6
marketingbacanero-byte <marketingbacanero@gmail.com>
2026-05-16 19:21:27 -0500

    Add files via upload

---
+++ calculadora/calculadoraAPP/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('calculadora/',views.calculadoraV,name="calculadoraU"),  
+    path('suma/', views.sumaV, name='sumaU'),
+    path('resta/', views.restaV, name='restaU'),
+    path('multiplicacion/', views.multiplicacionV, name='multiplicacionU'),
+    path('division/', views.divisionV, name='divisionU'),    
+]
---
+++ calculadora/calculadoraAPP/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+# Create your views here.
+def calculadoraV(request):  
+    return render(request,"calculadoraApp/index.html")
+
+def obtener_numeros(request): #recupero los numeros ingresados en pantalla
+    n1 = request.POST.get('n1', 0)
+    n2 = request.POST.get('n2', 0)
+    return float(n1), float(n2), n1, n2
+#Vista de la suma
+def sumaV(request):
+    num1, num2, n1, n2 = obtener_numeros(request)
+    resultado = num1 + num2
+    context = {'resultado': resultado, 'n1': n1, 'n2': n2}
+    return render(request, "calculadoraApp/index.html", context)
+   
+# Vista de la Resta
+def restaV(request):
+    num1, num2, n1, n2 = obtener_numeros(request)
+    resultado = num1 - num2
+    context = {'resultado': resultado, 'n1': n1, 'n2': n2}
+    return render(request, "calculadoraApp/index.html", context)
+   
+# Vista de la Multiplicación
+def multiplicacionV(request):
+    num1, num2, n1, n2 = obtener_numeros(request)
+    resultado = num1 * num2
+    context = {'resultado': resultado, 'n1': n1, 'n2': n2}
+    return render(request, "calculadoraApp/index.html", context)
+   
+# Vista de la División
+def divisionV(request):
+    num1, num2, n1, n2 = obtener_numeros(request)
+    if num2 == 0:
+        resultado = "Error: División por 0"
+    else:
+        resultado = num1 / num2
+    context = {'resultado': resultado, 'n1': n1, 'n2': n2}
+    return render(request, "calculadoraApp/index.html", context)
+
+
+
+   
